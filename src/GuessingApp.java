1d5d89fe6a565ee0f93a50c4dbfc6581ae24032c
BL-Yeswanth <yeswanth.polisetti@bridgelabz.com>
2026-02-06 10:59:15 +0530

    [yeswanth] Added:UC1: Iguessing game configuration Generated random target number

--- src/GuessingApp.java
+++ src/GuessingApp.java
@@ -1,6 +1,10 @@
 public class GuessingApp {
 
     public static void main(String[] args) {
+
         System.out.println("Welcome to the Guessing App");
+
+        GameConfig gameConfig = new GameConfig();
+        gameConfig.showRules();
     }
 }
